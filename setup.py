d52cf32c58b7137365c3a129c33f0183bb8f64bd
Florent Kermarrec <florent@enjoy-digital.fr>
2018-02-22 10:19:47 +0100

    uniformize litex cores

--- setup.py
+++ setup.py
@@ -5,30 +5,31 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-if sys.version_info[:3] < (3, 3):
-    raise SystemExit("You need Python 3.3+")
+if sys.version_info[:3] < (3, 5):
+    raise SystemExit("You need Python 3.5+")
 
 
 setup(
-	name="liteusb",
-	version="0.1",
-	description="small footprint and configurable USB core",
-	long_description=open("README").read(),
-	author="Florent Kermarrec",
-	author_email="florent@enjoy-digital.fr",
-	url="http://enjoy-digital.fr",
-	download_url="https://github.com/enjoy-digital/liteusb",
+    name="liteusb",
+    version="0.1",
+    description="Small footprint and configurable USB core",
+    long_description=open("README").read(),
+    author="Florent Kermarrec",
+    author_email="florent@enjoy-digital.fr",
+    url="http://enjoy-digital.fr",
+    download_url="https://github.com/enjoy-digital/liteusb",
+    test_suite="test",
     license="BSD",
     platforms=["Any"],
     keywords="HDL ASIC FPGA hardware design",
-	classifiers=[
-		"Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
-		"Environment :: Console",
-		"Development Status :: Alpha",
-		"Intended Audience :: Developers",
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+        "Environment :: Console",
+        "Development Status :: Alpha",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
-		"Operating System :: OS Independent",
-		"Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
     ],
     packages=find_packages(),
     include_package_data=True,
